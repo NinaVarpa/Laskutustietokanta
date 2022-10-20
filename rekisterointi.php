dbee91514cac6deff7c34caacaeabaa80685335b
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2022-10-20 13:32:56 +0300

    Create rekisterointi.php

---
+++ rekisterointi.php
@@ -0,0 +1,16 @@
+<html>
+    <form action ="kasittely.php" method ="post">
+          <div class="otsikko2">Rekisteröidy</div><br>
+          <label for="nimi"> Nimi </label>
+          <input class="lomake" type="text" name="Nimi"><br>
+          <label for="salasana"> Salasana </label>
+          <input class="lomake" type="text" name="Salasana"><br>
+          <label for="Tunnus"> Tunnus </label>
+          <input class="lomake" type="text" name="Tunnus"><br>
+          <label for="Osoite"> Osoite </label>
+          <input class="lomake" type="text" name="Osoite"><br>
+          <label for="Tunnusid"> Tunnusid </label>
+          <input class="lomake" type="text" name="Tunnusid"><br>
+          <input class="lomaketieto"type="submit"value="Rekisteröidy">
+        </form>
+    </html>
