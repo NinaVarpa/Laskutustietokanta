da8b0b7ea3036ae4817a8340107a50ad70efb17b
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2022-12-17 19:52:51 +0200

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,15 @@
+<html> 
+    <h1>LASKUTUSOHJELMA</h1>  
+      <form action ="kasittely2.php" method ="post"><br>
+          <label for="nimi"> Nimi </label>
+          <input type="text" name="Nimi"><br>
+          <label for="salasana"> Salasana </label>
+          <input type="text" name="Salasana"><br>
+          <input type="submit" value="Kirjaudu">
+        </form>
+
+        
+  <a href="kirjaus.php"> lomake</a><br>
+  <a href= "rekisterointi.php"> Rekisteröidy</a>
+  </Body>
+</html>
