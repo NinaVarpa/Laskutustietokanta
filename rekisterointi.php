100fc98580fd5f25b3bf80834ac7d6ed9a04d92c
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2022-12-20 23:00:51 +0200

    Update rekisterointi.php

--- rekisterointi.php
+++ rekisterointi.php
@@ -5,12 +5,7 @@
           <input class="lomake" type="text" name="Nimi"><br>
           <label for="salasana"> Salasana </label>
           <input class="lomake" type="text" name="Salasana"><br>
-          <label for="Tunnus"> Tunnus </label>
-          <input class="lomake" type="text" name="Tunnus"><br>
-          <label for="Osoite"> Osoite </label>
-          <input class="lomake" type="text" name="Osoite"><br>
-          <label for="Tunnusid"> Tunnusid </label>
-          <input class="lomake" type="text" name="Tunnusid"><br>
+          <br>
           <input class="lomaketieto"type="submit"value="Rekisteröidy">
         </form>
     </html>
