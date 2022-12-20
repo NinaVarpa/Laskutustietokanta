e5d07566f432f07063c8e59501da9e7b98d53e95
NinaVarpa <88774708+NinaVarpa@users.noreply.github.com>
2022-12-20 22:59:59 +0200

    Update kasittely.php

--- kasittely.php
+++ kasittely.php
@@ -1,7 +1,11 @@
 <?php
-include 'rekisterointi.php'
+include 'rekisterointi.php';
+$servername = "hyvis.mysql.database.azure.com";
+$username = "db_projekti";
+$password = "Sivyh2022";
+$dbname = "ninav_db";
 
-//mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli ($servername, $username, $password, $dbname);
  if ($conn->connect_error) {
   die("connection failed: " .$conn-> connect_error);
@@ -9,19 +13,15 @@ $conn = new mysqli ($servername, $username, $password, $dbname);
 
     
     
-//echo $_POST['Nimi'];
-//echo $_POST['Salasana'];
-//echo $_POST['Tunnus'];
-//echo $_POST['Osoite'];
-//echo $_POST['Tunnusid']
+echo $_POST['Nimi'];
+echo $_POST['Salasana'];
+
 $nimi = $_POST['Nimi'];
 $salasana = $_POST['Salasana'];
-$tunnus = $_POST['Tunnus'];
-$osoite = $_POST['Osoite'];
-$tunnusid =$_POST['Tunnusid']
-//stmt = $conn->prepare('INSERT INTO kartoittaja (Tunnus, nimi, osoite, tunnusid,salasana)VALUES (?,?,?,?,?)');
-//$stmt->bind_param('sssss', $Tunnus,$nimi,$osoite,$tunnusid,$salasana);
-//$stmt-> execute();
-//header("Location: index.php");
-//die();
+
+$stmt = $conn->prepare('INSERT INTO kartoittaja(nimi,salasana)VALUES(?,?)');
+$stmt->bind_param('ss',$nimi,$salasana,);
+$stmt-> execute();
+header("Location: index.php");
+die();
 ?>
